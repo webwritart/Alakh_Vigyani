2d07851405c9da2ea49c454eefa4b998e2dacdf6
Shwetabh <webwritat@gmail.com>
2025-12-01 14:49:02 +0530

    add coming soon and team login

--- routes/main.py
+++ routes/main.py
@@ -1,10 +1,55 @@
 from flask import Blueprint, render_template, request, flash, session, url_for, redirect
 from extensions import current_year
-
+from operations.miscellaneous import generate_captcha
 
 main = Blueprint('main', __name__, static_folder='static', template_folder='templates/main')
 
 
 @main.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html', current_year=current_year)
+    session['url'] = request.url
+    if 'view' not in session:
+        return render_template('coming_soon.html', current_year=current_year)
+    else:
+        return render_template('index.html', current_year=current_year)
+
+
+@main.route('/team_login', methods=['GET', 'POST'])
+def team_login():
+    password = 'b00800m'
+    captcha_value, captcha_uri = generate_captcha()
+    session['captcha_value'] = captcha_value
+    session['pwd'] = password
+    return render_template('view_login.html', captcha=captcha_uri, current_year=current_year)
+
+
+@main.route('/captcha_verification', methods=['GET', 'POST'])
+def captcha_verification():
+    if request.method == 'POST':
+        captcha_value = session.get('captcha_value')
+        if request.form.get('captcha') != captcha_value:
+            flash("Wrong Captcha!", "error")
+        else:
+            # name = request.form.get('name')
+            # email = request.form.get('email')
+            # email2 = request.form.get('email2')
+            # msg = request.form.get('message')
+            # if email == email2:
+            #     message = f'{msg}\n\n\nSENDER DETAILS:\nName: {name}\nEmail: {email}\n'
+            #
+            #     success_msg = (f'Dear {name},\n\nThanks for sending me a message.\nI will get back to you as soon as possible. '
+            #                f':)\n\n\nShwetabh Suman\nConcept Artist & Illustrator\nNew Delhi, India')
+            #     send_email('IMPORTANT!! - Main Portfolio', ['shwetabhartist@gmail.com'], email, message, '', '')
+            #     send_email('MESSAGE SENT - Shwetabh Suman', [email], 'shwetabhartist@gmail.com', success_msg, '', '')
+            #     flash('Message sent successfully!', 'success')
+            # else:
+            #     flash("The email doesn't match!", "error")
+    # check entered password and allow view--------------------------------------------------------------------
+            password = session['pwd']
+            if request.form.get('pwd') == password:
+                session['view'] = 'allow'
+                print('pwd matched and view added to session')
+                return redirect(session.get('url'))
+            else:
+                flash("Wrong Password!", "error")
+    return redirect(session.get('url'))
