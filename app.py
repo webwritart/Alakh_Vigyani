07327fcaf53b9f39e0e15e6bdb6dd50691c54dbe
Shwetabh <webwritat@gmail.com>
2025-12-03 15:21:56 +0530

    add database

--- app.py
+++ app.py
@@ -1,26 +1,30 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
-from extensions import mail
+from extensions import mail, db, login_manager
 from routes.main import main
 from routes.school import school
 from routes.writings import writings
 from routes.about import about
-from routes.spiritual import spiritual
+from routes.spiritual import spiritual, retreats
 from routes.photography import photography
-
+from models.member import Member, Role
 
 
 load_dotenv()
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('APP_SECRET')
+# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///alakh.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_USE_SSL'] = True
 mail.init_app(app)
+db.init_app(app)
+login_manager.init_app(app)
 
 
 app.register_blueprint(main, url_prefix='/')
@@ -29,6 +33,13 @@ app.register_blueprint(spiritual, url_prefix='/spiritual')
 app.register_blueprint(school, url_prefix='/school')
 app.register_blueprint(writings, url_prefix='/writings')
 app.register_blueprint(about, url_prefix='/about')
+
+with app.app_context():
+    db.create_all()
+
+@login_manager.user_loader
+def load_user(member_id):
+ return db.get_or_404(Member, member_id)
 
 
 if __name__ == '__main__':
--- extensions.py
+++ extensions.py
@@ -1,6 +1,12 @@
 from flask_mail import Mail, Message
 from datetime import datetime
+from flask_mail import Mail, Message
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, current_user
 
 
+db = SQLAlchemy()
+login_manager = LoginManager()
 mail = Mail()
+
 current_year = datetime.now().year
